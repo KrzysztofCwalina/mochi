b61ebb9991e8ca70832f9f20a88362a678ce90d2
Krzysztof Cwalina <kcwalina@microsoft.com>
2023-06-21 10:34:05 -0700

    CLU/OpenAI integration works

--- mochi/Mochi.Code/TextToCode.cs
+++ mochi/Mochi.Code/TextToCode.cs
@@ -3,6 +3,7 @@
 
 using Azure.AI.OpenAI;
 using Azure.FX.AI;
+using Azure.FX.Tooling;
 using Mochi;
 
 static partial class Program
@@ -18,9 +19,10 @@ static partial class Program
 
             // this is the OpenAI system message
             _context = $$"""
-                You are a C# programmer. 
-                When you show me code, I want just code. No markup, no markdown, not comments, etc.
                 You have the following C# API available: {{api}}. 
+                I will ask you to call one of the avaliable methods that corresponds the closest to your answer. 
+                When you generate C# code, I want just code; no markup, no markdown, no commentary, etc. as I will be compiling the code.
+                If none of the specific APIs match, call Code.FreeFormAnswer and pass your free form textual response as the method's argument.
             """;
         }
 
@@ -28,15 +30,15 @@ static partial class Program
         {
             int retries = 5;
             var prompt = new Prompt(_context);
-            prompt.Add($"Show me code (just code) to compute {request}.");
+            prompt.Add($"Show me code (just code) to compute {request}");
 
             while (retries-- > 0)
             {
                 string response = await ai.GetAnswerAsync(prompt);
-                Console.WriteLine("LOG: " + response);
+                Cli.WriteLine("LOG: " + response, ConsoleColor.DarkBlue);
                 var error = ExecutionRuntime.MakeCall(response, allow);
                 if (error == null) return;
-                Console.WriteLine("LOG: " + error);
+                Cli.WriteLine("LOG: " + error, ConsoleColor.DarkBlue);
                 prompt.Add($"I got the following error {error} when compiling the code. Can you fix the code you provided previously?", ChatRole.User);
             }
         }
--- mochi/Program.cs
+++ mochi/Program.cs
@@ -16,9 +16,8 @@ public static class Code
     public static void GetWeatherForecast(double longitude, double latitude, DateTimeOffset when) => Console.WriteLine($"it's nice");
 
     public static void GetTime(double longitude, double latitude) => Console.WriteLine($"It's {DateTime.Now.ToString("t")}");
-    public static void DontUnderstand() => Console.WriteLine("I don't understand the request");
-
-    public static void CommunicateWithUser(string textToSpeak) => Console.WriteLine(textToSpeak);
 
     public static void Add(double x, double y) => Console.WriteLine(x + y);
+
+    public static void FreeFormAnswer(string text) => Console.WriteLine(text);
 }
