138faf094ffbbc923e62e1ae0ef119c212d3cf07
Krzysztof Cwalina <kcwalina@microsoft.com>
2023-06-27 10:11:32 -0700

    added mock server endpoint

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,19 @@
+using Azure.FX.Applications;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+builder.Services.AddCloudMachine();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Mochi Server");
 
 app.Run();
+
+app.MapGet("api/todos", (CloudMachineRequest request) => {
+    return Results.Ok(
+        new { 
+            todos = new object [] {
+                new { task="hire electrician", assignedTo="Ela" }
+            }
+        }
+    );
+});
